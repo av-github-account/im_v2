a82b4269b501ffcd30230d1dbcd490f986a256fb
av-github-account <av.avramenko@gmail.com>
2020-04-02 18:48:27 +0300

    Урок #3 сделан

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 class MainController extends Controller {
@@ -11,10 +12,13 @@ class MainController extends Controller {
   }
 
   public function categories() {
-    return view('categories');
+    $categories = Category::get();
+    return view('categories',compact('categories'));
   }
 
-  public function category($category = null) {
+  public function category($code) {
+    $category = Category::where('code',$code)->first();
+//    dd($categoryObject);
     return view('category',compact('category'));
   }
 
--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -48,34 +48,18 @@
 
 <div class="container">
     <div class="starter-template">
-                                    <div class="panel">
-            <a href="/mobiles">
+      @foreach($categories as $category)
+        <div class="panel">
+            <a href="/{{ $category->code }}">
                 <img src="http://internet-shop.tmweb.ru/storage/categories/mobile.jpg">
-                <h2>Мобильные телефоны</h2>
+                <h2>{{ $category->name }}</h2>
             </a>
             <p>
-                В этом разделе вы найдёте самые популярные мобильные телефонамы по отличным ценам!
+                {{ $category->description }}
             </p>
         </div>
-            <div class="panel">
-            <a href="/portable">
-                <img src="http://internet-shop.tmweb.ru/storage/categories/portable.jpg">
-                <h2>Портативная техника</h2>
-            </a>
-            <p>
-                Раздел с портативной техникой.
-            </p>
-        </div>
-            <div class="panel">
-            <a href="/appliances">
-                <img src="http://internet-shop.tmweb.ru/storage/categories/appliance.jpg">
-                <h2>Бытовая техника</h2>
-            </a>
-            <p>
-                Раздел с бытовой техникой
-            </p>
-        </div>
-        </div>
+      @endforeach
+    </div>
 </div>
 </body>
 </html>
